18018526b709eca8fef16bd7ec98a6fb8e13d743
fengjiamin <fengjm3@lenovo.com>
2017-12-27 18:16:43 +0800

    add model

--- shop/admin.py
+++ shop/admin.py
@@ -2,5 +2,19 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Category,Product
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    #使用prepopulated_fields 属性来指定那些要使用其他字段来自动赋值的字段
+    prepopulated_fields = {'slug':('name',)}
+admin.register(Category,CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'stock',
+                    'available', 'created', 'updated']
+    list_filter = ['available','created','updated']
+    list_editable = ['price','stock','available']
+    prepopulated_fields = {'slug':('name',)}
+admin.register(Product,ProductAdmin)
