a488eb9ae701f8ebb41345ad527777f25870f986
fengjiamin <fengjm3@lenovo.com>
2018-01-02 16:24:25 +0800

    export csv;add custom action in adminsite

--- orders/admin.py
+++ orders/admin.py
@@ -3,11 +3,39 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from .models import Order,OrderItem
+import datetime
+import csv
+from django.http import HttpResponse
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 # Register your models here.
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
+
+def export_to_csv(modeladmin,request,queryset):
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename={}.csv'.format(opts.verbose_name)
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    # Write a first row with header information
+    writer.writerow([field.verbose_name for field in fields])
+    # Write data rows
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value,datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+export_to_csv.short_description = 'Export to CSV'
+
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
@@ -15,5 +43,6 @@ class OrderAdmin(admin.ModelAdmin):
                     'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
 
 admin.site.register(Order,OrderAdmin)
--- payment/apps.py
+++ payment/apps.py
@@ -6,3 +6,7 @@ from django.apps import AppConfig
 
 class PaymentConfig(AppConfig):
     name = 'payment'
+    verbose_name = 'Payment'
+
+    def ready(self):
+        import payment.signals
